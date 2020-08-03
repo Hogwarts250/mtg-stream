0bc67ef266c4f3609f13edd852740276992f9d2d
Felix Zhang <38085633+Hogwarts250@users.noreply.github.com>
2020-08-03 16:04:25 -0700

    update create/join with MediaStreams, add styling

--- streams/models.py
+++ streams/models.py
@@ -6,6 +6,6 @@ from functools import partial
 # Create your models here.
 class Game(models.Model):
     num_players = models.IntegerField(
-        validators=[MinValueValidator(2), MaxValueValidator(4)]
+        validators=[MinValueValidator(2), MaxValueValidator(4)],
     )
     game_id = models.CharField(max_length=16, unique=True)
